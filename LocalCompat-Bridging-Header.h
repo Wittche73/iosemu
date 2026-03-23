2b7831065820a130a560d8d3f4b5e1dfb3d2e5d6
fırat <f-rat@frat.73>
2026-03-23 23:27:19 +0300

    Fix macOS build: Remove os_workgroup_interval_t patch

--- LocalCompat-Bridging-Header.h
+++ LocalCompat-Bridging-Header.h
@@ -1,6 +1,3 @@
 // Removed harmful availability overrides
 #import <Foundation/Foundation.h>
 #include "RuntimeBridge.h"
-
-// SDK Patch: Define missing type to prevent hangs during module compilation
-typedef void* os_workgroup_interval_t;
